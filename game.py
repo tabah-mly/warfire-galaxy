bc377f2dd30c1f9936fcd9bd6257ccf044f42253
tabah-mly <tabahmulya917@gmail.com>
2026-02-13 01:12:49 +0800

    chore: first course finished

---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame, sys, random
+from utils.camera import Camera
+from utils.game_basic import GameBasic
+from utils.infinite_background import InfiniteBackground
+
+class Game(GameBasic):
+    def __init__(self):
+        pass
+
+    def event_listener(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self):
+        pass
+
+    def start(self):
+        pass
