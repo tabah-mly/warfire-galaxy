dc776b07c2f56d04167255bd2856ced4ca83965f
tabah-mly <tabahmulya917@gmail.com>
2026-04-13 08:45:35 +0800

    chore: second course finished

--- game.py
+++ game.py
@@ -4,17 +4,38 @@ from utils.game_base import GameBase
 from utils.infinite_background import InfiniteBackground
 
 class Game(GameBase):
-    def __init__(self):
-        pass
+    def __init__(self, width, height, title):
+        self.screen_width = width
+        self.screen_height = height
+        self.screen_title = title
+
+        self.initialize()
+
+        pygame.display.set_caption(title)
+
+        self.running = True
 
     def event_listener(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
     def update(self, dt):
         pass
 
     def draw(self):
+        self.screen.fill((24, 20, 37))
         pass
 
     def start(self):
-        pass
+        while self.running:
+            dt = self.clock.tick(60)
+
+            self.event_listener()
+            self.update(dt)
+            self.draw()
+
+            pygame.display.flip()
+
+        pygame.quit()
+        sys.exit()
